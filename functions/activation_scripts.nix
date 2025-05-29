9c900fd5ce3bb1911e35179054f09e11e3ccdfdb
Allemand Instable <68656923+code-instable@users.noreply.github.com>
2025-05-29 15:07:29 +0200

    (broken) update vscode extensions

--- functions/activation_scripts.nix
+++ functions/activation_scripts.nix
@@ -2,6 +2,24 @@
 # cf : https://github.com/LnL7/nix-darwin/blob/master/modules/system/activation-scripts.nix#L148-L155
 {pkgs, lib, config, ...}:
 let
+  # update_vscode_extensions-path = ../scripts/update_vscode_extensions.nu;
+  # 🚧 does not work because needs : `${pkgs.vscode}/bin/code` and not `code`
+
+  update_vscode_extensions-script-nu = /* nu */
+  ''
+  ['base' 'Default' 'document' 'dynamic' 'static' 'web-dev']
+  | each {
+    |profile|
+    ${pkgs.vscode}/bin/code --user-data-dir ${config.users.users.instable.home}/.vscode --update-extensions --profile \$profile
+  }
+  '';
+
+  update_vscode_extensions-script = /* zsh */
+  ''
+    printf "\n\033[1;33m⟩ updating vscode extensions: \n\033[0m" >&2
+    ${pkgs.nushell}/bin/nu -c "${update_vscode_extensions-script-nu}"
+  '';
+  
   # ⓘ note: _script in the name allows for syntax highlight
   xdg_open_symlink_script = /* zsh */
   ''
@@ -17,8 +35,9 @@ in
   # scripts defined within this file
   system.activationScripts = {
       xdg_open_symlink.text = xdg_open_symlink_script;
+      update_vscode_extensions.text = update_vscode_extensions-script;
   };
-
+  
 
   # very first script executed
   system.activationScripts.preActivation.text = lib.concatStringsSep "\n" [
@@ -26,10 +45,14 @@ in
   ];
 
   # very last script executed
-  system.activationScripts.postActivation.text = lib.concatStringsSep "\n" [
-    config.system.activationScripts.pkg_config-path.text
-    config.system.activationScripts.xdg_open_symlink.text
-    # system/macos-environment.nix
-    config.system.activationScripts.flushDNS.text
-  ];
+  system.activationScripts.postActivation.text = let
+    post_activation_scripts = with config.system.activationScripts; [
+      # pkg_config-path.text
+      pkgConfigPath.text
+      xdg_open_symlink.text
+      # system/macos-environment.nix
+      flushDNS.text
+      # update_vscode_extensions.text
+    ];
+  in lib.concatStringsSep "\n" post_activation_scripts;
 }
