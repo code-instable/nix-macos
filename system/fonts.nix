f6c807152aefd6742dfa6fffe4a6822cd200f436
Allemand Instable <68656923+code-instable@users.noreply.github.com>
2025-07-18 13:45:42 +0200

    added fonts

--- system/fonts.nix
+++ system/fonts.nix
@@ -12,6 +12,24 @@
     nerd-fonts.jetbrains-mono
     nerd-fonts.fira-code
     nerd-fonts.fira-mono
+    nerd-fonts.symbols-only
+    nerd-fonts.zed-mono
+    nerd-fonts.noto
+    nerd-fonts.tinos
+    nerd-fonts.lilex
+    nerd-fonts.arimo
+    nerd-fonts.lekton
+    nerd-fonts.go-mono
+    nerd-fonts.meslo-lg
+    nerd-fonts.geist-mono
+    nerd-fonts.victor-mono
+    nerd-fonts.roboto-mono
+    nerd-fonts.intone-mono
+    nerd-fonts.envy-code-r
+    nerd-fonts.atkynson-mono
+    nerd-fonts.sauce-code-pro
+    nerd-fonts.inconsolata-lgc
+    nerd-fonts.dejavu-sans-mono
     lmodern
     lmmath
     xits-math
